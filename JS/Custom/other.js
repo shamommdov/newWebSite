473fb6d7dc27a2cf89f7ab1468f94364fdcda315
shamomammadov90 <shamo.mammadov@outlook.com>
2022-07-25 16:02:38 +0400

    n1

---
+++ JS/Custom/other.js
@@ -0,0 +1,18 @@
+
+
+// logonun yanıb sönməsi
+// const webSiteLogo = document.querySelector("#webSiteLogo");
+// const logoColorArr = ["white", "#fa836a"];
+// var count = 0;
+// window.onload = function(){
+//     setInterval(function(){
+//         if(count == 2){
+//             count = 0;
+//         }
+//         let logo_color = logoColorArr[count];
+//         count++;
+//         webSiteLogo.style.color = logo_color;
+//     }, 1000);
+// }
+
+
